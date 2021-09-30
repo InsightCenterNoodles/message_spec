467217e3803ce450e1db9df6e0d581f47fc60409
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2021-09-30 15:15:21 -0600

    Rework of messages
    Object messages
    - Let objects have different representations in a nicer, extensible way
    
    Plot messages
    - Promote plots to first class entity, like tables

--- doc/snippets/object_m_defs.cpp
+++ doc/snippets/object_m_defs.cpp
@@ -10,10 +10,10 @@ void set_position(vec3 p)
 void set_rotation(vec4 q)
 void set_scale(vec3 s)
 
-void select_region(vec3, vec3, bool)
-void select_sphere(vec3, real, bool)
-void select_half_plane(vec3, vec3, bool)
-
+void select_region(vec3, vec3, int)
+void select_sphere(vec3, real, int)
+void select_half_plane(vec3, vec3, int)
+void select_hull([vec3], [int], int)
 [string, vec3] probe_at(vec3)
 
 void signal_attention()
